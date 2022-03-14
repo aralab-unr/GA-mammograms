3e0d2fd456416ed1dc508ac08fe4b7138751435e
Adarsh Sehgal <adarsh23.sehgal@gmail.com>
2022-03-13 17:43:31 -0700

    updating adarsh notes, adding script to convert DICOM images to PNG from INbreast database

---
+++ inbreast_preprocess_script.py
@@ -0,0 +1,14 @@
+import cv2
+import os
+import pydicom
+
+inputdir = '/home/adarshsehgal/Adarsh/INbreast Release 1.0/AllDICOMs/'
+outdir = '/home/adarshsehgal/Adarsh/INbreast Release 1.0/png images/'
+#os.mkdir(outdir)
+
+test_list = [ f for f in  os.listdir(inputdir)]
+
+for f in test_list:   # remove "[:10]" to convert all images
+    ds = pydicom.read_file(inputdir + f) # read dicom image
+    img = ds.pixel_array # get image array
+    cv2.imwrite(outdir + f.replace('.dcm','.png'),img) # write png image
