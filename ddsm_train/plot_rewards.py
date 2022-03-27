7aa48dedcd29e90d1f77d93b320297de332dfcb6
Adarsh Sehgal <adarsh23.sehgal@gmail.com>
2022-03-27 10:45:29 -0700

    adding python plot code, updating my notes, adding sample log files for GA

---
+++ ddsm_train/plot_rewards.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+aucArray=[]
+timesFitnessFuncEvaluated=[]
+
+if os.path.exists("logs_common.txt"):
+    with open('logs_common.txt', 'r') as f:
+        for line in f:
+            if 'AUC calculated ' in line:
+                aucArray.append(float(line[15:].replace('\n','')))
+
+for i in len(aucArray):
+    timesFitnessFuncEvaluated.append(i)
+
+x = np.array(timesFitnessFuncEvaluated)
+y = np.array(aucArray)
+
+plt.title("AUC vs. Times Fitness function evaluated")
+plt.plot(x, y)
+plt.xlabel('Times Fitness function evaluated')
+plt.ylabel('AUC')
+
+plt.savefig('AUC vs. Times Fitness function evaluated')
