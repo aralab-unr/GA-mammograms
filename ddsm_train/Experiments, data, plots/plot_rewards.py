cee8b9aa4fb70c7c608841a672c9eca163835957
Adarsh Sehgal <adarsh23.sehgal@gmail.com>
2022-03-28 13:22:26 -0700

    updating logs, experiment tracker

--- ddsm_train/Experiments, data, plots/plot_rewards.py
+++ ddsm_train/Experiments, data, plots/plot_rewards.py
@@ -1,6 +1,6 @@
 import os
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 
 aucArray=[]
 timesFitnessFuncEvaluated=[]
@@ -10,17 +10,18 @@ if os.path.exists("../logs_common.txt"):
         for line in f:
             if 'AUC calculated ' in line:
                 aucArray.append(float(line[15:].replace('\n','')))
+                print(float(line[15:].replace('\n','')))
 
 for i in range(0,len(aucArray)):
     timesFitnessFuncEvaluated.append(i)
 
-x = np.array(timesFitnessFuncEvaluated)
-y = np.array(aucArray)
-
-plt.title("AUC vs. Times Fitness function evaluated")
-plt.plot(x, y)
-plt.xlabel('Times Fitness function evaluated')
-plt.ylabel('AUC')
-
-plt.savefig('AUC vs. Times Fitness function evaluated')
+# x = np.array(timesFitnessFuncEvaluated)
+# y = np.array(aucArray)
+#
+# plt.title("AUC vs. Times Fitness function evaluated")
+# plt.plot(x, y)
+# plt.xlabel('Times Fitness function evaluated')
+# plt.ylabel('AUC')
+#
+# plt.savefig('AUC vs. Times Fitness function evaluated')
 
