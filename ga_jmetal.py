04380f013148f6d495466b6f47633c939901330e
Adarsh Sehgal <adarsh23.sehgal@gmail.com>
2022-04-20 09:02:24 -0700

    adding jmetal ga file

---
+++ ga_jmetal.py
@@ -0,0 +1,24 @@
+from jmetal.algorithm.multiobjective import NSGAII
+from jmetal.operator import SBXCrossover, PolynomialMutation
+from jmetal.problem import ZDT1
+from jmetal.util.termination_criterion import StoppingByEvaluations
+from jmetal.lab.visualization import Plot
+from jmetal.util.solution import get_non_dominated_solutions, print_function_values_to_file, \
+    print_variables_to_file
+
+problem = ZDT1()
+
+algorithm = NSGAII(
+    problem=problem,
+    population_size=100,
+    offspring_population_size=100,
+    mutation=PolynomialMutation(probability=1.0 / problem.number_of_variables, distribution_index=20),
+    crossover=SBXCrossover(probability=1.0, distribution_index=20),
+    termination_criterion=StoppingByEvaluations(max_evaluations=25000)
+)
+
+algorithm.run()
+
+front = get_non_dominated_solutions(algorithm.get_result())
+plot_front = Plot(title='Pareto front approximation', axis_labels=['x', 'y'])
+plot_front.plot(front, label='NSGAII-ZDT1', filename='NSGAII-ZDT1', format='png')
