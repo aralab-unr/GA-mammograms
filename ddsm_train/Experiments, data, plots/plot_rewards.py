6c4f8cef3960df2ac66363a9da33564f4a4a8bf0
Adarsh Sehgal <adarsh23.sehgal@gmail.com>
2022-05-02 07:47:04 -0700

    updating script to plot generation wise stats

--- ddsm_train/Experiments, data, plots/plot_rewards.py
+++ ddsm_train/Experiments, data, plots/plot_rewards.py
@@ -1,28 +1,46 @@
 import os
-# import numpy as np
-# import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
 
 aucArray=[]
 timesFitnessFuncEvaluated=[]
+population_size = 50
+generation_best = []
+generation_average = []
+generations_index_list = []
 
-if os.path.exists("../logs_common.txt"):
-    with open('../logs_common.txt', 'r') as f:
+if os.path.exists("logs_common-2.txt"):
+    with open('logs_common-2.txt', 'r') as f:
         for line in f:
             if 'AUC calculated ' in line:
                 aucArray.append(float(line[15:].replace('\n','')))
                 with open('logs_rewards.txt', 'a') as output:
                     output.write(str(float(line[15:].replace('\n',''))) + "\n")
 
-for i in range(0,len(aucArray)):
+for i in range(0, len(aucArray)):
     timesFitnessFuncEvaluated.append(i)
 
-# x = np.array(timesFitnessFuncEvaluated)
-# y = np.array(aucArray)
-#
-# plt.title("AUC vs. Times Fitness function evaluated")
-# plt.plot(x, y)
-# plt.xlabel('Times Fitness function evaluated')
-# plt.ylabel('AUC')
-#
-# plt.savefig('AUC vs. Times Fitness function evaluated')
+generations = len(aucArray)/population_size
+
+for i in range(0, int(generations)):
+    tempArray = aucArray[i*population_size:((i+1)*population_size)]
+    tempArray = np.array(tempArray)
+    generation_best.append(np.max(tempArray))
+    generation_average.append(np.average(tempArray))
+    generations_index_list.append(i+1)
+
+generation_best = np.array(generation_best)
+generation_average = np.array(generation_average)
+
+
+
+plot1 = plt.figure(1)
+plt.title("AUC over GA generations")
+plt.plot(generations_index_list, generation_best, label='Best fitness')
+plt.plot(generations_index_list, generation_average, label='Average fitness')
+plt.xlabel("Generations")
+plt.ylabel("Fitness value")
+plt.legend()
+
+plt.show()
 
